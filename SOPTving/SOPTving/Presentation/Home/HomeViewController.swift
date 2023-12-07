f1c31ed5c3ea00b6effe7875012c5dac995ca46f
MINJAE KIM <ffalswo2@naver.com>
2023-12-07 16:53:17 +0900

    test17

--- SOPTving/SOPTving/Presentation/Home/HomeViewController.swift
+++ SOPTving/SOPTving/Presentation/Home/HomeViewController.swift
@@ -14,6 +14,7 @@ protocol HomeViewControllerProtocol: AnyObject {
 
 final class HomeViewController: BaseViewController {
 
+    
     // MARK: - Properties
 
     private var viewModel: HomeViewModel
